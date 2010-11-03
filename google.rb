f327649b39270aeb8e20770d31b71e2c6a407530
Peter Aronoff <telemachus@arpinum.org>
2010-11-03 18:57:18 -0400

    Tweak regex following dominikh's advice

--- google.rb
+++ google.rb
@@ -6,17 +6,17 @@ require 'cgi'
 class Google
   include Cinch::Plugin
   plugin "google"
-  help "!g(oogle) [number] <query> - search Google"
-  match /g(?:oogle)? (.+)/
+  help "!g(oogle) [number] <query> - search Google (5 default results; give number for less)"
+  match /g(?:oogle)? (?:(\d+) )?(.+)/
 
-  def execute(m, query)
-    stop = 4
+  def execute(m, number, query)
+    number ||= 5
     query = query.split(/\s+/).join(" ")
 
     url = "http://www.google.com/search?q=#{CGI.escape(query)}"
 
     doc = Nokogiri::HTML(open(url))
-    results = doc.css('h3.r a.l')[0..(stop)]
+    results = doc.css('h3.r a.l')[0..(number.to_i - 1)]
 
     if results[0].nil?
       m.reply "No results found."
