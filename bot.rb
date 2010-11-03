0d2faa019e769508641bc1d396f5f0878797e6da
Peter Aronoff <telemachus@arpinum.org>
2010-11-03 15:45:16 -0400

    Add plugins; tweak help

--- bot.rb
+++ bot.rb
@@ -5,14 +5,16 @@ require 'help'
 require 'commands'
 require 'memo'
 require 'google'
+require 'debian'
+require 'dictionary'
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.oftc.net"
     c.channels = ["#debian-forums"]
-    c.nick = "name_here"
-    c.password = "secret_here"
-    c.plugins.plugins = [Help, Commands, Memo]
+    c.nick = "antinoos"
+    c.realname = "antinoos"
+    c.plugins.plugins = [Commands, Memo, Identify, Google, Help, Debian, Dictionary]
   end
 end
 
