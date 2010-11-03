f0278bd6b8c978d3a724588bd286ae2cf7af9d32
Peter Aronoff <telemachus@arpinum.org>
2010-11-03 12:19:10 -0400

    Refactor google plugin

--- google.rb
+++ google.rb
@@ -6,24 +6,30 @@ require 'cgi'
 class Google
   include Cinch::Plugin
   plugin "google"
-  help "!google <query> - run a search through Google"
-  match /google (.+)/
-
-  def search(query)
-    url = "http://www.google.com/search?q=#{CGI.escape(query)}&safe=active"
-
-    doc = Nokogiri::HTML(open(url))
-
-    link = doc.css('h3.r a.l').first
-    if !link.nil?
-      link.content
-    else
-      "No results found"
-    end
-  end
+  help "!g(oogle) [number] <query> - search Google"
+  match /g(?:oogle)? (.+)/
 
   def execute(m, query)
-    m.reply(search(query))
+    stop = 4
+    query = query.split(/\s+/).join(" ")
+
+    url = "http://www.google.com/search?q=#{CGI.escape(query)}"
+
+    doc = Nokogiri::HTML(open(url))
+    results = doc.css('h3.r a.l')[0..(stop)]
+
+    if results[0].nil?
+      m.reply "No results found."
+    else
+      results.each do |result|
+        if result.nil?
+          m.reply "No more results."
+          break
+        else
+          m.reply "#{result.text} (#{result['href']})"
+        end
+      end
+    end
   end
 end
 
